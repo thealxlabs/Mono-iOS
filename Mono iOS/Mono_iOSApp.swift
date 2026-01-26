5e3e7f9f90e1d328ad4338a36fe2f2b7a8fb64df
Alexander Wondwossen <alex.reed0@icloud.com>
2026-01-26 18:11:02 -0500

    Initial Commit

---
+++ Mono iOS/Mono_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mono_iOSApp.swift
+//  Mono iOS
+//
+//  Created by Alexander The Great on 2026-01-26.
+//
+
+import SwiftUI
+
+@main
+struct Mono_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
